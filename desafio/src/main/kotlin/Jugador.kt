82e3a8d768ff7f236cb04cbe07de204f5234ab2a
Alonso Codes Gavilán <acodes2000@educastillalamancha.es>
2021-10-02 11:42:05 +0200

    inicio desarrollo

---
+++ desafio/src/main/kotlin/Jugador.kt
@@ -0,0 +1,11 @@
+class Jugador {
+    var sector:Int = 0
+    var vida:Int = 0
+    var fuerza:Int = 0
+
+    constructor(){
+        this.sector = sector
+        this.vida = vida
+        this.sector = sector
+    }
+}
