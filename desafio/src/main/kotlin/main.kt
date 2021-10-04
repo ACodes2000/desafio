5f6a635379d659865c00c879809e83b6febdde63
Alonso Codes Gavilán <acodes2000@educastillalamancha.es>
2021-10-04 19:59:13 +0200

    desarrollo FINAL

--- desafio/src/main/kotlin/main.kt
+++ desafio/src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import kotlin.random.Random
+
 fun main(){
     var ganar:Boolean = false
     var tiempo:Int = 0
@@ -10,15 +12,44 @@ fun main(){
     var capitolio:Capitolio = Capitolio()
     println("LLenando la reserva de objetos......")
     capitolio.llenarReserva()
-    Thread.sleep(3000)
+    Thread.sleep(1000)
+    tiempo++
     var mapa:Mapa = Mapa()
     mapa.rellenarMapaItems(capitolio)
     mapa.rellenarMapaJugador()
     println("EMPIEZA EL JUEGO")
     mapa.mostrarMapa()
+    println(" ")
+    Thread.sleep(1000)
+    tiempo++
     do{
+        var n:Int = Random.nextInt(1,5)
         if(tiempo % 2 == 0){
+            if(n == 1) {
+                mapa.moverseIzq(capitolio)
+            }
 
+            if(n == 2) {
+                mapa.moverseArriba(capitolio)
+            }
+
+            if(n == 3) {
+                mapa.moverseDer(capitolio)
+            }
+
+            if(n == 4) {
+                mapa.moverseAbajo(capitolio)
+            }
+            mapa.mostrarMapa()
+            println()
         }
-    }while(ganar == true)
+        Thread.sleep(1000)
+        tiempo++
+
+        if(tiempo % 5 == 0){
+            mapa.tirarCuatroItems(capitolio)
+        }
+        Thread.sleep(1000)
+        ganar = mapa.ganador()
+    }while(ganar == false)
 }
