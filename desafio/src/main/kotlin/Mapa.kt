62ce37a483e886283438b5ac86844b969d9bb636
Alonso Codes Gavilán <acodes2000@educastillalamancha.es>
2021-10-04 17:47:48 +0200

    desarrollo v5

--- desafio/src/main/kotlin/Mapa.kt
+++ desafio/src/main/kotlin/Mapa.kt
@@ -1,9 +1,45 @@
+import kotlin.random.Random
+
 class Mapa {
     var mapa = Array(5) { Array<Any>(5){" "} }
 
-    fun rellenarMapa(c:Capitolio){
+    fun rellenarMapaItems(c:Capitolio){
+        var contador:Int = 0
         for(i in 0..mapa.size-1){
+            for(x in 0..mapa.size-1){
+                while(contador!=11) {
+                    var r1 = Random.nextInt(0, 5)
+                    var r2 = Random.nextInt(0, 5)
+                    var r3 = Random.nextInt(c.reservaItems.size)
+                    mapa[r1][r2] = c.reservaItems[r3].descripcion
+                    contador++
+                    c.reservaItems.removeAt(r3)
+                }
+            }
+        }
+    }
 
+    fun rellenarMapaJugador(){
+        for(i in 0..mapa.size-1){
+            for(x in 0..mapa.size-1){
+                var contador = 0
+                var r1 = Random.nextInt(mapa.size)
+                var r2 = Random.nextInt(mapa.size)
+                if(mapa[r1][r2] == " "){
+                    mapa[r1][r2] = Factoria.jugadores[contador].sector
+                    Factoria.jugadores.removeAt(contador)
+                }
+            }
+        }
+    }
+
+    fun mostrarMapa(){
+        for(i in 0..mapa.size-1) {
+            for (x in 0..mapa.size - 1) {
+                print("|"+mapa[i][x]+"|")
+                print(" ")
+            }
+            println("")
         }
     }
 }
--- desafio/src/main/kotlin/main.kt
+++ desafio/src/main/kotlin/main.kt
@@ -1,4 +1,6 @@
 fun main(){
+    var ganar:Boolean = false
+    var tiempo:Int = 0
     Factoria.crearJugadoresSector1()
     Factoria.crearJugadoresSector2()
     Factoria.crearJugadoresSector3()
@@ -6,5 +8,17 @@ fun main(){
     Factoria.crearJugadoresSector5()
 
     var capitolio:Capitolio = Capitolio()
+    println("LLenando la reserva de objetos......")
     capitolio.llenarReserva()
+    Thread.sleep(3000)
+    var mapa:Mapa = Mapa()
+    mapa.rellenarMapaItems(capitolio)
+    mapa.rellenarMapaJugador()
+    println("EMPIEZA EL JUEGO")
+    mapa.mostrarMapa()
+    do{
+        if(tiempo % 2 == 0){
+
+        }
+    }while(ganar == true)
 }
